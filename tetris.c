1950b40a62868bbba9d76939004b6706bbe900a3
N8theGr8M8 <152344336+N8theGr8M8@users.noreply.github.com>
2025-11-25 21:48:31 -0500

    Add files via upload

---
+++ tetris.c
@@ -0,0 +1,31 @@
+#include <ncurses.h> 
+#include <stdlib.h> 
+#include <unistd.h> // for usleep()
+#include <signal.h>
+#include <time.h> 
+
+struct Block {
+    char position[4][4];
+}
+
+struct Block blocks[7];
+
+blocks[0].position = {{'o','o'},
+                      {'o','o'}};
+
+blocks[1].position = {{'o','o','o','o'}};
+
+blocks[2].position = {{'o','o','o'},
+                      {' ',' ','o'}};
+
+blocks[3].position = {{'o','o','o'},
+                      {'o',' ',' '}};
+
+blocks[4].position = {{'o','o','o'},
+                      {' ','o',' '}};
+
+blocks[5].position = {{'o','o',' '},
+                      {' ','o','o'}};
+
+blocks[6].position = {{' ','o','o'},
+                      {'o','o',' '}};
