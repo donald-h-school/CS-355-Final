bde5199b22ae5caed95cc036623a52b99bea1dee
N8theGr8M8 <152344336+N8theGr8M8@users.noreply.github.com>
2025-11-26 10:44:09 -0500

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -5,35 +5,61 @@
 #include <time.h> 
 
 struct Block {
-    char position[4][4];
+    char structure[4][4];
+};
+
+struct Block cube = {.structure = {{'O','O'},
+                                   {'O','O'}}};
+
+struct Block line = {.structure = {{' ',' ',' ',' '},
+                                   {'O','O','O','O'},
+                                   {' ',' ',' ',' '},
+                                   {' ',' ',' ',' '}}};
+
+struct Block reverseL = {.structure = {{' ',' ',' '},
+                                       {'O','O','O'},
+                                       {' ',' ','O'}}};
+
+struct Block regL = {.structure = {{' ',' ',' '},
+                                   {'O','O','O'},
+                                   {'O',' ',' '}}};
+
+struct Block tee = {.structure = {{' ',' ',' '},
+                                  {'O','O','O'},
+                                  {' ','O',' '}}};
+
+struct Block regZ = {.structure = {{' ',' ',' '},
+                                   {'O','O',' '},
+                                   {' ','O','O'}}};
+
+struct Block reverseZ = {.structure = {{' ',' ',' '},
+                                       {' ','O','O'},
+                                       {'O','O',' '}}};
+
+struct Block current;
+
+int main() {
+    
 }
 
-struct Block blocks[7];
-
-blocks[0].position = {{'o','o'},
-                      {'o','o'}};
-
-blocks[1].position = {{' ',' ',' ',' '},
-                      {'o','o','o','o'},
-                      {' ',' ',' ',' '},
-                      {' ',' ',' ',' '}};
-
-blocks[2].position = {{' ',' ',' '},
-                      {'o','o','o'},
-                      {' ',' ','o'}};
-
-blocks[3].position = {{' ',' ',' '},
-                      {'o','o','o'},
-                      {'o',' ',' '}};
-
-blocks[4].position = {{' ',' ',' '},
-                      {'o','o','o'},
-                      {' ','o',' '}};
-
-blocks[5].position = {{' ',' ',' '},
-                      {'o','o',' '},
-                      {' ','o','o'}};
-
-blocks[6].position = {{' ',' ',' '},
-                      {' ','o','o'},
-                      {'o','o',' '}};
+void rotate() {
+    int a = sizeof(current.structure);
+    int b = sizeof(current.structure[0]);
+    for (int i = 0; i < a; i++) {
+        for (int j = i + 1; j < b; j++) {
+            int temp = current.structure[i][j];
+            current.structure[i][j] = current.structure[j][i];
+            current.structure[j][i] = temp;
+        }
+    }
+    for (int i = 0; i < a; i++) {
+        char temp[4];
+        int k = 0;
+        for (int j = b - 1; j >= 0; j--) {
+            temp[j] = current.structure[i][k];
+        }
+        for (int j = 0; j < b; j++) {
+            current.structure[i][j] = temp[j];
+        }
+    }
+}
