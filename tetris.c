1a32bdb95828f98c131cf5140ecafb00c1e8e106
N8theGr8M8 <152344336+N8theGr8M8@users.noreply.github.com>
2025-11-30 13:36:35 -0500

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -3,64 +3,123 @@
 #include <unistd.h> // for usleep()
 #include <signal.h>
 #include <time.h> 
+#include <string.h>
 
 struct Block {
-    char structure[4][4];
+    char structure[4][2];
 };
 
+
 struct Block cube = {.structure = {{'O','O'},
-                                   {'O','O'}}};
+                                   {'O','O'},
+                                   {'x','x'},
+                                   {'x','x'}}};
 
-struct Block line = {.structure = {{' ',' ',' ',' '},
-                                   {'O','O','O','O'},
-                                   {' ',' ',' ',' '},
-                                   {' ',' ',' ',' '}}};
+struct Block line = {.structure = {{'O','x'},
+                                   {'O','x'},
+                                   {'O','x'},
+                                   {'O','x'}}};
 
-struct Block reverseL = {.structure = {{' ',' ',' '},
-                                       {'O','O','O'},
-                                       {' ',' ','O'}}};
+struct Block reverseL = {.structure = {{'O','O'},
+                                       {'O','x'},
+                                       {'O','x'},
+                                       {'x','x'}}};
 
-struct Block regL = {.structure = {{' ',' ',' '},
-                                   {'O','O','O'},
-                                   {'O',' ',' '}}};
+struct Block regL = {.structure = {{'O','x'},
+                                   {'O','x'},
+                                   {'O','O'},
+                                   {'x','x'}}};
 
-struct Block tee = {.structure = {{' ',' ',' '},
-                                  {'O','O','O'},
-                                  {' ','O',' '}}};
+struct Block tee = {.structure = {{'O','x'},
+                                  {'O','O'},
+                                  {'O','x'},
+                                  {'x','x'}}};
 
-struct Block regZ = {.structure = {{' ',' ',' '},
-                                   {'O','O',' '},
-                                   {' ','O','O'}}};
+struct Block regZ = {.structure = {{'x','O'},
+                                   {'O','O'},
+                                   {'O','x'},
+                                   {'x','x'}}};
 
-struct Block reverseZ = {.structure = {{' ',' ',' '},
-                                       {' ','O','O'},
-                                       {'O','O',' '}}};
+struct Block reverseZ = {.structure = {{'O','x'},
+                                       {'O','O'},
+                                       {'x','O'},
+                                       {'x','x'}}};
 
 struct Block current;
+int angle = 0;
+int fall = 0;
+int LR = 5;
+int numb;
+
+void initialize();
+void handle_sigint(int sig);
+void rotate();
+void placeblock();
+void assignblock();
+void change_direction(int ch);
 
 int main() {
-    
+    signal(SIGINT, handle_sigint);
+    initialize();
+    refresh();
+    sleep(3);
+    endwin();
+    exit(0);
+}
+
+void initialize() {
+    initscr(); // Start ncurses mode
+    clear();
+    noecho(); // Do not show key presses
+    curs_set(0); // Hide the blinking cursor
+    keypad(stdscr, TRUE); // Enable arrow keys
+    nodelay(stdscr, TRUE); // Do not wait for key press
 }
 
 void rotate() {
-    int a = sizeof(current.structure);
-    int b = sizeof(current.structure[0]);
-    for (int i = 0; i < a; i++) {
-        for (int j = i + 1; j < b; j++) {
-            int temp = current.structure[i][j];
-            current.structure[i][j] = current.structure[j][i];
-            current.structure[j][i] = temp;
+    if (angle != 3) {
+        angle++;
+    }
+    else {
+        angle = 0;
+    }
+}
+
+void placeblock() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (current.structure[i][j] != 'x') {
+                if (angle == 0) {
+                    mvprintw(fall+i, LR+j, "%c", current.structure[i][j]);
+                }
+                else if (angle == 1) {
+                    mvprintw(fall+j, LR-i, "%c", current.structure[i][j]);
+                }
+                else if (angle == 2) {
+                    mvprintw(fall-i, LR-j, "%c", current.structure[i][j]);
+                }
+                else if (angle == 3) {
+                    mvprintw(fall-j, LR+i, "%c", current.structure[i][j]);
+                }
+            }
         }
     }
-    for (int i = 0; i < a; i++) {
-        char temp[4];
-        int k = 0;
-        for (int j = b - 1; j >= 0; j--) {
-            temp[j] = current.structure[i][k];
-            k++;
-        }
-        for (int j = 0; j < b; j++) {
-            current.structure[i][j] = temp[j];
-        }
+}
+
+void assignblock () {
+    for (int k = 0; k < 4; k++) {
+        strcpy(current.structure[k], regL.structure[k]);
+    }
+}
+
+void handle_sigint(int sig) { // Print a message and exit cleanly
+    endwin(); // End ncurses session
+    printf("Game terminated\n");
+    exit(0); // Exit the program
+}
+
+void change_direction(int ch) {
+    if (ch == KEY_RIGHT) {
+        rotate();
     }
 }
